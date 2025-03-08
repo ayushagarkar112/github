010764075d6f4f9c1cf0e2bce0b6a28d600c4ea5
AyushAgarkar <agarkarayush13@gmail.com>
2025-03-08 09:55:49 +0530

    checking indents

--- app.py
+++ app.py
@@ -21,17 +21,17 @@ while True:
     print(f"Computer chose: {comp_choice}")
 if user_choice == comp_choice:
         print("It's a tie! 🤝🏻")
-    elif(user_choice == 'rock' and comp_choice == 'scissors'):
+elif(user_choice == 'rock' and comp_choice == 'scissors'):
         print("Congratulations! You Won!! 🎉")
         u_points += 1
-    elif(user_choice == 'scissors' and comp_choice == 'paper'):
+elif(user_choice == 'scissors' and comp_choice == 'paper'):
         print("Congratulations! You Won!! 🎉")
         u_points += 1
-    elif(user_choice == 'paper' and comp_choice == 'rock'):
+elif(user_choice == 'paper' and comp_choice == 'rock'):
         print("Congratulations! You Won!! 🎉")
         u_points += 1
-    else:
+else:
         print("Oops! You Lose 😔")
         c_points += 1
     
-    print(f"Score - You: {u_points}, Computer: {c_points}\n")
+print(f"Score - You: {u_points}, Computer: {c_points}\n")
